10881f5d07cb0e96355bd2cff0d56c26df468a44
Jared Thibodeaux <admin@baynecorp.com>
2019-05-06 10:16:43 -0500

    Added Firebase Auth state changing to check if user is signed in or not.  Depending on that state will show different components.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import { View } from 'react-native';
 import firebase from 'firebase';
-import { Header } from './components/common';
+import { Header, Button, Spinner, Card, CardSection } from './components/common';
 import LoginForm from './components/LoginForm';
 
 class App extends Component {
+	state = { loggedIn: null }
+
 	componentWillMount() {
 		firebase.initializeApp({
 			apiKey: 'AIzaSyAth2XcnehKF_f8OXERX_0RM7d_FgmKA6I',
@@ -15,12 +17,38 @@ class App extends Component {
 			messagingSenderId: '131904684065',
 			appId: '1:131904684065:web:0117901d7d0c1bf2'
 		});
+
+		firebase.auth().onAuthStateChanged((user) => {
+			if (user) {
+				this.setState({ loggedIn: true });
+			} else {
+				this.setState({ loggedIn: false });
+			}
+		});
 	}
+
+	renderContent() {
+		switch (this.state.loggedIn) {
+			case true:
+				return (
+						<Card>
+							<CardSection>
+								<Button onPress={() => firebase.auth().signOut()}>Log Out</Button>
+							</CardSection>
+						</Card>
+						);
+			case false:
+				return <LoginForm />;
+			default:
+				return <Spinner size='large' />;
+		}
+	}
+
 	render() {
 		return (
 			<View>
 				<Header headerText="Authentication" />
-				<LoginForm />
+				{this.renderContent()}
 			</View>
 			);
 	}
